4f5a67ddc149256adfd641422fc2a44b2837d8f5
Alvyn Fasuyi <zetagik@gmail.com>
2024-07-14 08:16:20 +0200

    test cors

--- main.py
+++ main.py
@@ -12,23 +12,6 @@ from fastapi.responses import StreamingResponse
 
 app = FastAPI()
 
-# origins = ["*"]
-
-app.add_middleware(
-    # CORSMiddleware,
-    # allow_origins=origins,
-    # allow_credentials=True,
-    # # allow_methods=["*"],
-    # allow_headers=["*"],
-    # expose_headers=["*"],
-
-    CORSMiddleware,
-    allow_origins=['null'],
-    allow_credentials=True,
-    allow_methods=['*'],
-    allow_headers=['*']
-)
-
 @app.get('/stream/{dbid}')
 async def stream(dbid:str = '',s:int=None,e:int=None,l:str='eng'):
     if dbid:
