07d135589bd6c9f9e0533843d870ebf4eb073016
Alvyn Fasuyi <zetagik@gmail.com>
2024-07-15 23:16:42 +0200

    test

--- models/filemoon.py
+++ models/filemoon.py
@@ -17,7 +17,7 @@ async def handle(url) -> dict:
     # request = await fetch(SRC_URL)
     key= 'scp-live-9bc4c7e194dd4a19b92213ce243f8243'
     url = f"https://api.scrapfly.io/scrape?tags=player%2Cproject%3Adefault&asp=true&render_js=true&key={key}&url={SRC_URL}"
-    request = requests.request("GET", url)
+    request = await requests.request("GET", url)
     content = request.json()['result']['content']
     # content = request.text
     processed_matches = await process_packed_args(content)
