cc8996ffa7955923cc4b662ef210bb0fb265391c
Alvyn Fasuyi <zetagik@gmail.com>
2024-07-15 23:13:14 +0200

    test

--- models/filemoon.py
+++ models/filemoon.py
@@ -1,5 +1,6 @@
 from .utils import fetch
 import re
+import requests
 from .decoders.packed import *
 from . import subtitle
 async def handle(url) -> dict:
@@ -13,8 +14,13 @@ async def handle(url) -> dict:
     subtitles = await subtitle.vscsubs(SUB_URL)
 
 	# GET SRC
-    request = await fetch(SRC_URL)
-    processed_matches = await process_packed_args(request.text)
+    # request = await fetch(SRC_URL)
+    key= 'scp-live-9bc4c7e194dd4a19b92213ce243f8243'
+    url = f"https://api.scrapfly.io/scrape?tags=player%2Cproject%3Adefault&asp=true&render_js=true&key={key}&url={SRC_URL}"
+    request = requests.request("GET", url)
+    content = request.json()['result']['content']
+    # content = request.text
+    processed_matches = await process_packed_args(content)
     unpacked = await unpack(*processed_matches)
     hls_url = re.search(r'file:"([^"]*)"', unpacked).group(1)
     return {
